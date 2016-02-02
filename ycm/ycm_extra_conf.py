703e16a6575cb7325716395fd06c835ebd168913
Caleb Donovick <cdonovi1@binghamton.edu>
2016-02-02 15:06:01 -0500

    Add -Wno-unused-parameter and -Wno-unused-variable

--- ycm/ycm_extra_conf.py
+++ ycm/ycm_extra_conf.py
@@ -38,14 +38,16 @@ flags = [
 '-Wall',
 '-Wextra',
 '-Werror',
-'-Wpedantic',
+'-pedantic',
+'-Wno-unused-variable',
+'-Wno-unused-parameter',
 # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
 # language to use when compiling headers. So it will guess. Badly. So C++
 # headers will be compiled as C headers. You don't want that so ALWAYS specify
 # a "-std=<something>".
 # For a C project, you would set this to something like 'c99' instead of
 # 'c++11'.
-'-std=c++11',
+'-std=c++14',
 # ...and the same thing goes for the magic -x option which specifies the
 # language that the files to be compiled are written in. This is mostly
 # relevant for c++ headers.
