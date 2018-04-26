22e410d3838a491654d89f6114a70fd00cca1b96
donovick <donovick@cs.stanford.edu>
2018-04-26 22:29:15 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,15 @@
 rc=$(pwd)/vimrc.vim 
 dir=$(pwd)
 cd ~
+if [ -f .vimrc ] ; then
+	rm .vimrc
+fi
 ln -s $rc ~/.vimrc
+
+if [ -f .vim ] ; then
+	rm .vim
+fi
 ln -s $dir ~/.vim
 cd $dir
+
+git clone https://github.com/VundleVim/Vundle.vim.git $dir/bundle/Vundle.vim
